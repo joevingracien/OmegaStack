850c048ae427271a3110356d67d70a20a9b74b8d
joBar <jg.getintouch@gmail.com>
2024-02-13 14:44:07 +0100

    Revert "no idea what im doing"
    This reverts commit c6ad3c8dfa199c465d1592b06ea9a752a3dd4087.

--- remix/app/components/Post.tsx
+++ remix/app/components/Post.tsx
@@ -4,9 +4,7 @@ import { PortableText } from "@portabletext/react";
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityDocument } from "@sanity/client";
 
-import { initializeProjectDetails } from '../sanity/projectDetails';
-
-const { projectId, dataset, studioUrl, stegaEnabled } = initializeProjectDetails(context);
+import { projectId, dataset } from "../sanity/projectDetails";
 
 const builder = imageUrlBuilder({ projectId, dataset });
 
