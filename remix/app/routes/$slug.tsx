0b195972753b9219593268c99a06d588c5d0cdf9
joBar <jg.getintouch@gmail.com>
2024-02-13 14:44:22 +0100

    Revert "test"
    This reverts commit 2e06e4a7a13c25774d35219759b58cd1572ed5a8.

--- remix/app/routes/$slug.tsx
+++ remix/app/routes/$slug.tsx
@@ -1,5 +1,3 @@
-// Assuming initializeProjectDetails is exported from projectDetails.ts
-import { initializeProjectDetails } from '../sanity/projectDetails';
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import type { SanityDocument } from "@sanity/client";
@@ -9,12 +7,8 @@ import { useQuery } from "../sanity/loader";
 import { loadQuery } from "../sanity/loader.server";
 import { POST_QUERY } from "../sanity/queries";
 
-export const loader = async ({ params, context }: LoaderFunctionArgs) => {
-  // Initialize project details using the context
-  const { projectId, dataset } = initializeProjectDetails(context);
-
-  // Use projectId and dataset as needed, for example, to modify POST_QUERY or fetch data
-  const initial = await loadQuery<SanityDocument>(POST_QUERY, { ...params, projectId, dataset });
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const initial = await loadQuery<SanityDocument>(POST_QUERY, params);
 
   return { initial, query: POST_QUERY, params };
 };
