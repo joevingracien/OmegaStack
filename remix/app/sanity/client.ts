6c38e062c20d427b0293ab7868cff95815922d76
joBar <jg.getintouch@gmail.com>
2024-02-14 17:08:22 +0100

    client.ts inital node

--- remix/app/sanity/client.ts
+++ remix/app/sanity/client.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@sanity/client";
-import { getProjectDetails } from "./projectDetails";
-
-const { projectId, dataset, studioUrl, stegaEnabled } = getProjectDetails(env);
+import { stegaEnabled, projectId, dataset, studioUrl } from "./projectDetails";
 
 // Do not import this into client-side components unless lazy-loaded
 export const client = createClient({
